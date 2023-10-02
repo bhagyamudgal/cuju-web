ee6b387c5bc79b339015e1919aa5280537e717b1
Bhagya Mudgal <bhagyamudgal@gmail.com>
2023-10-03 00:17:04 +0530

    style: add feature section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import FeaturesSection from "../components/home-page/FeaturesSection";
 import HeroSection from "../components/home-page/HeroSection";
 
 function Home() {
-    return <HeroSection />;
+    return (
+        <>
+            <HeroSection />
+            <FeaturesSection />
+        </>
+    );
 }
 
 export default Home;
---
+++ src/components/home-page/FeaturesSection/index.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+
+import PageContainer from "../../common/PageContainer";
+import featureImage1 from "@/public/images/feature-1.svg";
+import featureImage2 from "@/public/images/feature-2.svg";
+import featureImage3 from "@/public/images/feature-3.svg";
+
+const FEATURES = [
+    {
+        name: "Donations Received",
+        value: 428,
+        image: featureImage1,
+    },
+    {
+        name: "Careers Started",
+        value: 84,
+        image: featureImage2,
+    },
+    {
+        name: "Amount Gathered",
+        value: "$24800",
+        image: featureImage3,
+    },
+];
+
+function FeaturesSection() {
+    return (
+        <section className="py-6">
+            <PageContainer className="flex flex-wrap justify-center">
+                {FEATURES.map((feature) => {
+                    return (
+                        <div className="bg-gradient-1 m-4 flex w-full max-w-[400px] items-center space-x-4 rounded-[48px] p-8">
+                            <Image src={feature.image} alt={feature.name} />
+                            <div className="space-y-3">
+                                <h4 className="text-xl">{feature.name}</h4>
+                                <p className="text-6xl font-bold">
+                                    {feature.value}
+                                </p>
+                            </div>
+                        </div>
+                    );
+                })}
+            </PageContainer>
+        </section>
+    );
+}
+
+export default FeaturesSection;
