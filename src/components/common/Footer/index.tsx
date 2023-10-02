0faec87d37f7a6688385d62cb09128630decf67b
Bhagya Mudgal <bhagyamudgal@gmail.com>
2023-10-02 23:38:11 +0530

    style: update footer

--- src/components/common/Footer/index.tsx
+++ src/components/common/Footer/index.tsx
@@ -10,14 +10,24 @@ function Footer() {
         <footer>
             <PageContainer className="flex items-end justify-between">
                 <div className="space-y-1">
-                    <Image src={solanaLogo} alt="solana-logo" />
+                    <Link
+                        href="https://solana.com"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <Image src={solanaLogo} alt="solana-logo" />
+                    </Link>
                     <p>Made possible on the Solana network.</p>
                     <p>
                         &copy; 2023 Dean&apos;s List DAO. All rights reserved.
                     </p>
                 </div>
 
-                <Link href="https://twitter.com/deanslistDAO">
+                <Link
+                    href="https://twitter.com/deanslistDAO"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
                     <Image src={twitterIcon} alt="twitter-icon" />
                 </Link>
             </PageContainer>
